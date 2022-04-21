b7ff4731bec568cac1b27ff73afee77f93e300a3
DO72 <michaellaw@gmx.com>
2022-04-21 13:42:20 -0400

    Code Challenge start

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div className="search">
+      {/* For the advanced deliverables: add a checkbox to allow sorting the planeteer */}
+    </div>
+  );
+}
+
+export default SearchBar;
